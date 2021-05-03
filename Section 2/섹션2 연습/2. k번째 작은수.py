391edb1b082cf67cf6502a124201d50c6018b7c3
Grace Chung <jhjyj5414@naver.com>
2021-05-03 09:19:15 +0900

    210503 k번째 작은수, k번째 큰수

--- Section 2/섹션2 연습/2. k번째 작은수.py
+++ Section 2/섹션2 연습/2. k번째 작은수.py
@@ -1,10 +1,17 @@
+# T = int(input())
+
+# for t in range(T):  
+#     N, s, e, k = map(int, input().split())
+
+#     arr = list(map(int, input().split()))
+#     arr = arr[s-1:e].sort()
+
+#     print('#' + t+1 + ' ' + arr[k-1])
+
 T = int(input())
 
-for t in range(T):  
-    N, s, e, k = map(int, input().split())
-
-    arr = list(map(int, input().split()))
-    arr = arr[s-1:e].sort()
-
-    print('#' + t+1 + ' ' + arr[k-1])
-
+for i in range(T):
+    n, s, e, k = map(int, input().split())
+    num_list = list(map(int, input().split()))
+    answer = sorted(num_list[s-1:e])[k-1]
+    print("#{} {}".format(i+1, answer))
--- Section 2/섹션2 연습/3. k번째 큰 수.py
+++ Section 2/섹션2 연습/3. k번째 큰 수.py
@@ -1,14 +1,20 @@
-N, k = map(int, input().split())
+# N, k = map(int, input().split())
 
+# arr = list(map(int, input().split()))
+
+# result = set()
+# result = [arr[i]+arr[j]+arr[r] for i in range(N) for j in range(i+1, N) for r in range(j+1, N)]
+
+# #print(result)
+
+# result = list(result)
+# result.sort(reverse=True)
+
+# #print(result)
+# print(result[k-1])
+
+N, k = map(int, input().split())
 arr = list(map(int, input().split()))
 
-result = set()
-result = [arr[i]+arr[j]+arr[r] for i in range(N) for j in range(i+1, N) for r in range(j+1, N)]
-
-#print(result)
-
-result = list(result)
-result.sort(reverse=True)
- 
-#print(result)
-print(result[k-1])
+sum_list = list(set([arr[i]+arr[j]+arr[k] for i in range(N) for j in range(i+1, N) for k in range(j+1, N)]))
+print(sorted(sum_list, reverse=True)[k-1])
